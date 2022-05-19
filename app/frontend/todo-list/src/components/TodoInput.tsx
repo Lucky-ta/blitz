4ba94a202d6a6c0c8150a10ed4dfd33814f23973
lucas <lucmaieski@gmail.com>
2022-05-18 22:14:18 -0300

    Arrumando css responsivo

--- app/frontend/todo-list/src/components/TodoInput.tsx
+++ app/frontend/todo-list/src/components/TodoInput.tsx
@@ -1,9 +1,13 @@
 import '../css/todoInput.css'
+import {FiPlusCircle} from "react-icons/fi";
 
 function TodoInput() {
     return(
         <div className='text-todo-input-container'>
             <input className="text-todo-input" type="text" placeholder="ADICIONE UMA TAREFA"/>
+            <button className='add-todo-btn'>
+                <FiPlusCircle color='red' size={30}/>
+            </button>
         </div>
     )
 }
