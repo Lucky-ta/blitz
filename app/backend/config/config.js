c91e432a257565deb026fe8e3cbf140ed6ea3aa7
lucas <lucmaieski@gmail.com>
2022-05-19 18:26:31 -0300

    initial commit backend

---
+++ app/backend/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": '27031977l',
+    "database": "todo-list",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    define: {
+      timestamps: false
+    }
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
---
+++ app/backend/migrations/20220519212007-create-task.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      task: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
