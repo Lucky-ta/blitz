3d2cdc77910f3a2d3e84ba128d4d4c42062066d5
lucas <lucmaieski@gmail.com>
2022-05-19 16:47:53 -0300

    Finalizando css

--- app/frontend/todo-list/src/components/Todos.tsx
+++ app/frontend/todo-list/src/components/Todos.tsx
@@ -1,26 +1,48 @@
 import '../css/todos.css'
+import { VscChromeClose } from "react-icons/vsc";
+import { VscCheckAll } from "react-icons/vsc";
 
 function Todos() {
     return(
         <div className="todos-container">
             <div className='task-container'>
+                <button className='check-btn'>
+                    <VscCheckAll size={30}/>
+                </button>
                 <p>Lavar louça</p>
-                <button className='exclude-btn'>X</button>
+                <button className='exclude-btn'>
+                    <VscChromeClose size={30}/>
+                </button>
             </div>
 
             <div className='task-container'>
+                <button className='check-btn'>
+                    <VscCheckAll size={30}/>
+                </button>
                 <p>Escovar os dentes</p>
-                <button className='exclude-btn'>X</button>
+                <button className='exclude-btn'>
+                    <VscChromeClose size={30}/>
+                </button>
             </div>
 
             <div className='task-container'>
+                <button className='check-btn'>
+                    <VscCheckAll size={30}/>
+                </button>
                 <p>Fazer almoço</p>
-                <button className='exclude-btn'>X</button>
+                <button className='exclude-btn'>
+                    <VscChromeClose size={30}/>
+                </button>
             </div>
 
             <div className='task-container'>
+                <button className='check-btn'>
+                    <VscCheckAll size={30}/>
+                </button>
                 <p>Estudar</p>
-                <button className='exclude-btn'>X</button>
+                <button className='exclude-btn'>
+                    <VscChromeClose size={30}/>
+                </button>
             </div>
         </div>
     )
