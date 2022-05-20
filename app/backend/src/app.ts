62f84ce0f4495672722d1df67b423308c6a7a940
lucas <lucmaieski@gmail.com>
2022-05-19 23:05:30 -0300

    Fazendo deploy

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,8 +1,9 @@
 import express, { Express } from 'express';
 import taskRouter from './routes/taskRouter';
+import cors from 'cors'
 
 const app: Express = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/', taskRouter);
