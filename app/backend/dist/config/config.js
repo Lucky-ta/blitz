9f1d4c27e433baff4add7f76f9d171351d31be52
lucas <lucmaieski@gmail.com>
2022-05-20 00:12:52 -0300

    Add dist

--- app/backend/dist/config/config.js
+++ app/backend/dist/config/config.js
@@ -1,10 +1,11 @@
 "use strict";
+require('dotenv').config();
 module.exports = {
     "development": {
-        "username": "root",
-        "password": '27031977l',
-        "database": "todo_list",
-        "host": "127.0.0.1",
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
         "dialect": "mysql",
         define: {
             timestamps: false
--- app/backend/dist/src/app.js
+++ app/backend/dist/src/app.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const taskRouter_1 = __importDefault(require("./routes/taskRouter"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use('/', taskRouter_1.default);
 exports.default = app;
